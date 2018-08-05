02788c97a6356257610bd93efbd319b65cde424e
syedzubairahmed001 <syedzubairahmed001@gmail.com>
2018-08-05 19:13:45 +0530

    changed the colors and the heroImage

--- resourses/js/script-index.js
+++ resourses/js/script-index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.menu-icon').click(function(e){
         $('nav ul').slideToggle();
+        $('#menu-icon-img').attr('src','resourses/img/cancel.svg');
         e.preventDefault();
     })
 })
