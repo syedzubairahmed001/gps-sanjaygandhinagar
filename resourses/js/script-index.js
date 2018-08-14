6e4678be0e9465559f3b209403530d4902591e57
syedzubairahmed001 <syedzubairahmed001@gmail.com>
2018-08-14 18:39:21 +0530

    added responsivity

--- resourses/js/script-index.js
+++ resourses/js/script-index.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     $('.menu-icon').click(function(e){
         $('nav ul').slideToggle();
-        $('#menu-icon-img').attr('src','resourses/img/cancel.svg');
+        $('.boss-container').click(function(){
+            $('nav ul').slideUp();
+            $('.menu-icon-img').toggleClass('menu-active');
+        });
+        $('.menu-icon-img').toggleClass('menu-active');
         e.preventDefault();
-    })
+    });
 })
